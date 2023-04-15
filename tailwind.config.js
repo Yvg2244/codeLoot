83b596fe6f0dfb97ec032306697c4621934373f8
Yvg2244 <yvgupta24@gmail.com>
2023-04-15 23:00:51 +0530

    npm dep installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "monte": ["Montserrat", "sans-serif"],
+        "poppins": ["Poppins", "sans-serif"],
+        "roboto": ["Roboto", "sans-serif"],
+        "inter":['Inter', 'sans-serif'],
+        
+      },
+      colors:{
+        primary_gray:"#18181B",
+        primary_gray_light:"#333337",
+        primary_black:"#151716",
+        primary_green:"#A9FF1C"
+
+      }
+    },
   },
   plugins: [],
-}
-
+};
